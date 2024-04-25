f2a704db426983da7a07230f5cdec109585dccb5
Bibo Boom <77632683+BiboBoom10@users.noreply.github.com>
2024-04-25 19:54:11 +0300

    Add stats

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,6 @@
 const HttpError = require('../models/http-error');
 const Technician = require('../models/technician');
+const Order = required('../models/order');
 
 exports.main = async (req, res, next) => {
     try {
@@ -17,5 +18,17 @@ exports.getTechnicians = async (req, res, next) => {
     } catch (err) {
         console.log(err);
         return next(new HttpError('Unable to get technicians'))
+    }
+}
+
+exports.stats = async (req, res, next) => {
+    try {
+        const orders = await Order.find({ 'recepient?._id': req.id });
+        const successfullOrders = orders.filter(e => e.status === 'Accepted');
+        const failedOrders = orders.filter(e => e.status === 'Rejected');
+        res.status(200).json({ message: 'Statistics', orders, successfullOrders, failedOrders })
+    } catch (err) {
+        console.log(err);
+        return next(new HttpError('Unable to get statistics'));
     }
 }
--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const mainController = require('../controllers/main');
 const authRouter = require('./auth');
+const { accessToken } = require('../middleware/access-token')
 const notificationsRouter = require('./notifications');
 
 const router = express.Router();
@@ -9,6 +10,8 @@ const router = express.Router();
 router.get('/', mainController.main);
 
 router.get('/technicians', mainController.getTechnicians);
+
+router.get('/statistics', accessToken, mainController.stats);
 
 router.use('/auth', authRouter);
 
